2c8bbd3985bdf1b6d9e8f2951538c017dbd0493d
Justin <justinpxng@gmail.com>
2018-11-20 06:13:31 +0000

    Join between close price and dividend data

--- iex.R
+++ iex.R
@@ -1,4 +1,4 @@
-library(devtools)
+library(tidyverse)
 
 tickers <- c("aapl")
 endpoints <- c("chart", "dividends")
@@ -9,14 +9,22 @@ url_endpoint <- function(endpoint, ticker, time_window){
 
 
 df_iex_hist <- function(endpoint, ticker, time_window = "5y"){
-
   resp <- httr::GET(url_endpoint(endpoint, ticker, time_window))
   resp_parsed <- jsonlite::fromJSON(httr::content(resp, "text"))
   return(data.frame(resp_parsed))
-
 }
 
-df_iex_hist(endpoints[1], tickers[1])
-df_iex_hist(endpoints[2], tickers[1])
+returns_divd_adjd <- function(df_chart, df_divd){
+  df_divd %>%
+    rename(date = exDate) %>%
+    right_join(df_chart, by = "date") %>%
+    select(date, close, amount)
+}
+
+returns_divd_adjd(df_iex_hist(endpoints[1], tickers[1]), df_iex_hist(endpoints[2], tickers[1]))
+
+str(df_iex_hist(endpoints[1], tickers[1]))
+
+str(df_iex_hist(endpoints[2], tickers[1]))
 
 
